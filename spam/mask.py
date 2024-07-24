7a3267287ceb377bd9f2fde020b2155c3a1831a8
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-07-24 22:46:28 +0700

    Fix

--- spam/mask.py
+++ spam/mask.py
@@ -7,3 +7,21 @@ def mask(str, maska):
         return maska
     except Exception as err:
         logger.error(f"{err}")
+
+def format_phone(phone, phone_mask):
+    phone_list = list(phone)
+    for i in phone_list:
+        phone_mask = phone_mask.replace("#", i, 1)
+    return phone_mask
+
+def parse_phone(phone):
+    if len(phone) in [10, 11, 12]:
+        if phone[0] == "+":
+            phone = phone[1:]
+        elif phone[0] == "8":
+            phone = "7" + phone[1:]
+        elif phone[0] == "9":
+            phone = "7" + phone
+        return phone
+    else:
+        pass
